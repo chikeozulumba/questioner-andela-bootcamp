89beabdd17ab4a11d82d91998458182898faa31b
Chike <chike.ozulumba@gmail.com>
2018-12-20 05:54:25 +0100

    [Feature #162723805] Create Landing Page

---
+++ UI/assets/js/index.js
@@ -0,0 +1,10 @@
+(function () {
+  function togggleSidebar (e) {
+    e.preventDefault()
+    const sideBar = document.getElementById('sidebar')
+    sideBar.classList.toggle('side-active')
+    sideBar.classList.toggle('side-hidden')
+  }
+  const menu = document.getElementById('menu')
+  menu.addEventListener('click', togggleSidebar, false)
+})()
