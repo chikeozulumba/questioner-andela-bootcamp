f7e95e2d03f1c281348c6edd06892a2ba636675d
Chike <chike.ozulumba@gmail.com>
2019-01-03 16:20:35 +0100

    [Feature #162953305] Implement response handler for 5xx errors

--- src/functions/handlers.js
+++ src/functions/handlers.js
@@ -7,6 +7,15 @@ export function error4xx(response, code, status, message) {
 	});
 }
 
+export function error5xx(response, code, status, message) {
+	return response.status(code).send({
+		status,
+		error: {
+			message,
+		},
+	});
+}
+
 export function response2xx(response, code, status, data) {
 	return response.status(code).send({
 		status,
