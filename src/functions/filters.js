d0c846c32c193ec4a7383f303684008b9bc10b14
Chike <chike.ozulumba@gmail.com>
2019-01-04 09:29:31 +0100

    [Feature #162969799] Implement Date Sorting

--- src/functions/filters.js
+++ src/functions/filters.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 const sortArrayById = (collection, id) => {
 	id = parseInt(id, 10);
 	for (let i = 0; i < collection.length; i += 1) {
@@ -6,8 +8,17 @@ const sortArrayById = (collection, id) => {
 	}
 	return null;
 };
+
+const date = (array) => {
+	array.sort((current, next) => {
+		const currentDate = moment(current.happeningOn, 'YYYY-DD-MM').format();
+		const nextDate = moment(next.happeningOn, 'YYYY-DD-MM').format();
+		return new Date(currentDate).getTime() - new Date(nextDate).getTime();
+	});
+	return array;
+};
 const last = array => array[array.length - 1];
 
-const Filters = { sortArrayById, last };
+const Filters = { sortArrayById, last, date };
 
 export default Filters;
