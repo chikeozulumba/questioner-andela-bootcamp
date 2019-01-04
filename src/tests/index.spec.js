adcf5014daf089574bc90fb999d87befa4e1e53d
Chike <chike.ozulumba@gmail.com>
2019-01-04 21:39:45 +0100

    [Chore #162994382] Refactor mocha test

--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -6,7 +6,20 @@ import app from '../app';
 const { expect } = chai;
 chai.use(chaiHttp);
 
-describe('User should be able to visit Frontend pages', () => {
+describe('User should be able to visit Base App route', () => {
+	it('should return status 200', (done) => {
+		chai
+			.request(app)
+			.get('/')
+			.end((err, res) => {
+				if (err) throw err;
+				expect(res).to.have.status(200);
+				done();
+			});
+	});
+});
+
+describe('User should be able to visit Base API route', () => {
 	it('should return status 200', (done) => {
 		chai
 			.request(app)
