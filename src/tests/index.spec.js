b36dc0f256787e78e0dfbc525c37fb9d183a762a
Chike <chike.ozulumba@gmail.com>
2019-01-04 21:47:54 +0100

    [Chore #162994382] Add headers in Mocha tests

--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -31,3 +31,28 @@ describe('User should be able to visit Base API route', () => {
 			});
 	});
 });
+
+describe('User should recieve error message on invalid route', () => {
+	it('should return status 404 on accepting HTML', (done) => {
+		chai
+			.request(app)
+			.get('/api/v1/abrakadabra')
+			.set('Accept', 'application/xhtml+xml')
+			.end((err, res) => {
+				if (err) throw err;
+				expect(res).to.have.status(404);
+				done();
+			});
+	});
+	it('should return status 404 on accepting JSON', (done) => {
+		chai
+			.request(app)
+			.get('/api/v1/abrakadabra')
+			.set('Accept', 'application/json')
+			.end((err, res) => {
+				if (err) throw err;
+				expect(res).to.have.status(404);
+				done();
+			});
+	});
+});
