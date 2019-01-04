9affb53c899b4e289566fbddd00810f66f9ff0dc
Chike <chike.ozulumba@gmail.com>
2019-01-04 10:13:20 +0100

    [Chore #162979928] Modify route description  in controllers

--- src/routes/v1.js
+++ src/routes/v1.js
@@ -14,49 +14,49 @@ router.use(morgan('combined', {
 	skip: (req, res) => res.statusCode < 400,
 }));
 
-// @route GET /api/v1/meetups
-// @desc  Create meetup route
+// @route POST /api/v1/meetups
+// @desc  Create meetup
 // @access public
 
 router.post('/meetups', Meetup.create);
 
-// @route GET /api/v1/questions
-// @desc  Create question route
+// @route POST /api/v1/questions
+// @desc  Create question
 // @access public
 
 router.post('/questions', Question.create);
 
 // @route GET /api/v1/meetups/<meetup-id>
-// @desc  Create question route
+// @desc  Get Specific Meetup record
 // @access public
 
 router.get('/meetups/:id', Meetup.getRecord);
 
-// @route GET /api/v1/meetups/<meetup-id>
-// @desc  Create question route
+// @route GET /api/v1/meetups/
+// @desc  Get all meetups
 // @access public
 
 router.get('/meetups/', Meetup.getAllRecords);
 
 // @route PATCH /api/v1/questions/<question-id>/upvote
-// @desc  Upvote a question route
+// @desc  Upvote a question
 // @access public
 
 router.patch('/questions/:id/upvote', Question.vote);
 
 // @route PATCH /api/v1/questions/<question-id>/downvote
-// @desc  Downvote a question route
+// @desc  Downvote a question
 // @access public
 
 router.patch('/questions/:id/downvote', Question.vote);
 
 // @route PATCH /api/v1/meetups/<meetup-id>/rsvp
-// @desc  RSVP for a meetup route
+// @desc  RSVP for a meetup
 // @access public
 
 router.post('/meetups/:id/rsvp', Meetup.rsvp);
 
-// @route PATCH /api/v1/questions/<question-id>/downvote
+// @route PATCH /api/v1/meetups/upcoming
 // @desc  Fetch all upcoming meetup records
 // @access public
 
